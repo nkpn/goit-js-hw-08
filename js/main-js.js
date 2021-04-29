ac4b1fde105e8b918806dccc143908833ea56341
nkpn <illinmikita@gmail.com>
2021-04-29 22:10:36 +0300

    go go go

--- js/main-js.js
+++ js/main-js.js
@@ -1,10 +1,11 @@
 import gallery from './gallery-items.js';
-console.log(gallery);
 
 const galleryEl = document.querySelector('.js-gallery');
-const lightboxEl = document.querySelector('.js-lightbox');
+const lightboxEl = document.querySelector('.lightbox__content');
+const modalEl = document.querySelector('[data-modal]');
+const btnCloseEl = document.querySelector('.lightbox__button');
 
-
+//* Загрузка картинок
 const pictureUploadFunction = gallery.map(image => {
     return `<li class="gallery__item">
     <a
@@ -22,3 +23,30 @@ const pictureUploadFunction = gallery.map(image => {
 }).join('');
 
 galleryEl.insertAdjacentHTML('afterbegin', pictureUploadFunction);
+
+
+//* Открытие модального окна
+function openModal(event) {
+    event.preventDefault(); //* убрали перезагрузку
+    modalEl.classList.add('is-open');
+    lightboxEl.classList.add('is-open');
+}
+
+
+//* каждой картинке по слушателю!
+const imagesEl = document.querySelectorAll('.gallery__link');
+
+imagesEl.forEach((image) => {
+    image.addEventListener('click', openModal);
+});
+
+
+
+//* закрытие модального окна
+function closeModal(event) {
+    event.preventDefault();
+    modalEl.classList.remove('is-open');
+    lightboxEl.classList.remove('is-open');
+}
+
+btnCloseEl.addEventListener('click', closeModal);
